3a70b0e5cbdd8b11b214f2eb507bd0c1a540a6df
Kushagra Agarwal <kushagra.agarwal.2709@gmail.com>
2024-07-12 17:05:52 +0530

    feat: added checkVerified middleware

--- src/services/auth/authMiddleware.js
+++ src/services/auth/authMiddleware.js
@@ -21,4 +21,29 @@ const authenticate = (req, res, next) => {
   }
 };
 
-module.exports = { authenticate }
+const checkVerified = (req, res, next) => {
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    if (!token) throw createError.Unauthorized("No token provided");
+
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+    req.user = {
+      isVerified: true,
+      email: decoded.email
+    };
+    next();
+
+  }catch(error){
+
+    req.user = {
+      isVerified: false
+    }
+    
+    next()
+  }
+};
+
+module.exports = { authenticate, checkVerified }
